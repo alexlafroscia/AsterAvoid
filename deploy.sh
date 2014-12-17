5e5b9bfda022d4ca2eb2435ab7c5bb1d1e24afd2
Alex LaFroscia <alex@lafroscia.com>
2014-12-16 23:44:58 -0500

    Add deployment steps with Travis
    https://github.com/steveklabnik/automatically_update_github_pages_with_travis_example

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rev=$(git rev-parse --short HEAD)
+
+cd dist
+
+git init
+git config user.name "Alex LaFroscia"
+git config user.email "alex@lafroscia.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/alexlafroscia/cubefield-myo.git"
+git fetch upstream && git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "Rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
